b11a89374fceab912c2961bddcd251274b7fe9bc
Margret Riegert <margret@eowyn.net>
2022-05-17 17:25:22 -0400

    Better UI, activity and developer panes, redirects

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 import requests as rq
 from pprint import pprint
 from pathlib import Path
@@ -19,49 +19,80 @@ def favicon():
 
 @app.route('/projects/<proj>/')
 @app.route('/projects/<proj>/<sub>/')
+def redirectLongProj(proj, sub='summary'):
+   return redirect(f'/p/{proj}/{sub}/')
+
+@app.route('/p/<proj>/summary/')
+def redirectProjSummary(proj):
+   return redirect(f'/p/{proj}/')
+
 @app.route('/p/<proj>/')
 @app.route('/p/<proj>/<sub>/')
 def viewProject(proj, sub='summary'):
 
    sum_resp = rq.get(f"{SF_API_URL}/p/{proj}")
+
    if sub != "summary":
       sub_resp = rq.get(f"{SF_API_URL}/p/{proj}/{sub}")
    else:
       sub_resp = sum_resp
 
-   sum_json = sum_resp.json()
-   sum_json = formatProjSummaryJson(sum_json)
-
-   if Path(f'templates/proj_{sub}.html').exists() and 200 <= sub_resp.status_code < 500:
-      return render_template(f'proj_{sub}.html', proj=sum_json, sub_name=sub, sub=sub_resp.json())
-   elif 400 <= sub_resp.status_code < 500:
-      return render_template(f'proj_summary.html', proj=sum_json, sub_name=sub, sub=sub_resp.status_code)
+   if sub == "summary":
+      act_json = rq.get(f"{SF_API_URL}/p/{proj}/activity/").json()
    else:
-      return render_template(f'proj_summary.html', proj=sum_json, sub_name=sub, sub=sub_resp.json())
+      act_json = None
 
-def formatProjSummaryJson(proj):
-   tools = {}
-   first_tools = ['summary', 'files', 'reviews', 'support', 'wiki', 'bugs']
+   sum_json = formatProjJson(sum_resp.json())
 
-   for f in first_tools:
+   if 200 <= sub_resp.status_code < 300:
+      sub_json = formatProjJson(sub_resp.json())
+   else:
+      sub_json = None
+
+   if Path(f'templates/proj_{sub}.html').exists():
+      return render_template(f'proj_{sub}.html', proj=sum_json, sub_name=sub, sub=sub_json, recent_activity=act_json)
+   elif 400 <= sub_resp.status_code < 500:
+      return render_template(f'proj_summary.html', proj=sum_json, sub_name=sub, sub=sub_resp.status_code, recent_activity=act_json)
+   else:
+      return render_template(f'proj_summary.html', proj=sum_json, sub_name=sub, sub=sub_json, recent_activity=act_json)
+
+def formatProjJson(proj):
+   if 'tools' in proj:
+      tools = {}
+      first_tools = ['summary', 'files', 'wiki', 'bugs']
+      exclude_tools = ['activity', 'reviews', 'support']
+
+      for f in first_tools:
+         for tool in proj['tools']:
+            if f == tool['name']:
+               tools[f] = [tool]
+
       for tool in proj['tools']:
-         if f == tool['name']:
-            tools[f] = [tool]
-
-   for tool in proj['tools']:
-      if tool['name'] in tools:
-         if tool in tools[tool['name']]:
+         if tool['name'] in tools:
+            if tool in tools[tool['name']]:
+               continue
+            tools[tool['name']].append(tool)
+         elif tool['name'] in exclude_tools:
             continue
-         tools[tool['name']].append(tool)
-      else:
-         tools[tool['name']] = [tool]
+         else:
+            tools[tool['name']] = [tool]
 
-   tools['summary'][0]['url'] = tools['summary'][0]['url'].replace('summary/', '')
-   proj['tools'] = tools
+      tools['summary'][0]['url'] = tools['summary'][0]['url'].replace('summary/', '')
+      proj['tools'] = tools
+
+   if 'developers' in proj:
+      proj['developers'].sort(key=lambda item: item.get("username"))
+
+   if 'short_description' in proj:
+      proj['short_description'] = proj['short_description'].splitlines()
+
    return proj
 
 @app.route('/u/<user>/')
-@app.route('/u/<user>/profile')
+def redirectToUserProf(user):
+   return redirect(f'/u/{user}/profile/')
+
+@app.route('/u/<user>/profile/')
 def viewUserProfile(user):
    resp = rq.get(f"{SF_API_URL}/u/{user}/profile")
    return render_template('user.html', user=resp.json())
